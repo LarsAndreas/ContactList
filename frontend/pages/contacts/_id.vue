8fe03bc3ebde8e6402ad8011a2b876f5e2b5cf71
Lars Andreas Sørensen Vestbø <larsandreassovo@gmail.com>
2023-01-14 13:59:41 +0100

    Added Id pages

---
+++ frontend/pages/contacts/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <span>this is the contacts id page {{ id }}</span>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router/types/composables";
+
+const route = useRoute();
+
+const id = route.params.id;
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -159,4 +159,8 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+fetch("https://dummyjson.com/products/1")
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+</script>
