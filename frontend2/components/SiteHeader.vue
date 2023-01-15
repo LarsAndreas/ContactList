171022eaf3f5c56c00c96afcd0a84379c60a229f
Lars Andreas Sørensen Vestbø <larsandreassovo@gmail.com>
2023-01-15 15:43:55 +0100

    Added layout and SiteHeader

---
+++ frontend2/components/SiteHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-4 bg-white flex flex-row justify-between items-center">
+    <NuxtLink class="flex flex-row items-center text-gray-800" to="/">
+      <SvgPhoneOutline />
+      <span class="font-bold align-middle text-xl pl-2"> ContactList </span>
+    </NuxtLink>
+    <div class="flex flex-row items-center gap-4">
+      <div>
+        <img
+          class="w-8 h-8 rounded-full"
+          src="https://randomuser.me/api/portraits/men/79.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <NuxtLink class="flex flex-row text-gray-800" to="/login">
+          <span
+            class="px-4 py-2 bg-gray-200 text-gray-800 font-semibold rounded"
+          >
+            Login
+          </span>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend2/layouts/default.vue
+++ frontend2/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-screen h-screen bg-gray-100">
+    <SiteHeader />
     <div class="container w-full md:w-4/5 xl:w-3/5 mx-auto px-2">
       <slot></slot>
     </div>
